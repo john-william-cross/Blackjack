35629429dbf901d4d1f4ff275c7b52c0327827ef
John Cross <jwcross1@dmacc.edu>
2019-03-22 07:47:58 -0500

    lots of formatting changes...

--- src/edu/dmacc/codedsm/blackjack/BlackJack.java
+++ src/edu/dmacc/codedsm/blackjack/BlackJack.java
@@ -1,11 +1,8 @@
-
 package edu.dmacc.codedsm.blackjack;
-
 
 import java.util.*;
 
 public class BlackJack {
-
 
     public static void main(String[] args) {
         Map<String, List<Integer>> deck = new HashMap<>();
@@ -21,9 +18,9 @@ public class BlackJack {
 
         Card dealerFirst = dealCard(deck);
         Card dealerSecond = dealCard(deck);
-
         Card firstCard = dealCard(deck);
         Card secondCard = dealCard(deck);
+
         myHand.add(firstCard);
         dealerHand.add(dealerFirst);
         myHand.add(secondCard);
@@ -33,10 +30,11 @@ public class BlackJack {
 
         int dealerSum = dealerFirst.value + dealerSecond.value;
 
-        System.out.print("Player's cards are: ");
-        printCurrentHand(myHand);
-
         int sum = firstCard.value + secondCard.value;
+
+        System.out.println("Player's cards are: ");
+        printCurrentHand(myHand);
+        System.out.println();
 
         boolean isPlaying = true;
         while (isPlaying) {
@@ -46,13 +44,12 @@ public class BlackJack {
                 Card hit = dealCard(deck);
                 sum = sum + hit.value;
                 myHand.add(hit);
-                System.out.println();
-                System.out.println("Player's cards are: ");
+                System.out.println("\nPlayer hand is: ");
                 printCurrentHand(myHand);
+                System.out.println();
                 if (sum > 21) {
-                    System.out.println();
                     System.out.format("\nPlayer's hand was %d.", sum);
-                    System.out.println(" Player busts. Dealer wins!");
+                    System.out.format(" Player busts. %s wins!", "Dealer");
                     isPlaying = false;
                 }
 
@@ -64,40 +61,46 @@ public class BlackJack {
                     dealerSum = dealerSum + dealerHit.value;
                 }
                 if (dealerSum == 21) {
-                    System.out.println("Dealer's cards are: ");
+                    System.out.println("Dealer hand is: ");
                     printDealerHand(dealerHand);
                     System.out.format("Dealer's hand was %d", dealerSum);
-                    System.out.println("Dealer hit 21! Dealer wins!");
-                } else if (dealerSum > 16 && dealerSum > sum && dealerSum < 21) {
-                    System.out.print("Dealer's cards are: ");
-                    printDealerHand(dealerHand);
-                    System.out.format("Dealer's hand was %d", dealerSum);
-                    System.out.println();
-                    System.out.println();
-                    System.out.print("Player's hand is: ");
-                    printCurrentHand(myHand);
-                    System.out.println();
-                    System.out.format("Player's hand was %d. ", sum);
-                    System.out.println("Dealer wins!");
-                } else if (dealerSum == sum) {
-                    System.out.print("Dealer's cards are: ");
-                    printDealerHand(dealerHand);
-                    System.out.format("Dealer's hand was %d", dealerSum);
-                    System.out.print("\nPlayer's cards are: ");
-                    printCurrentHand(myHand);
-                    System.out.println();
-                    System.out.format("Player's hand was %d", sum);
-                    System.out.println("\nIt's a tie!");
-                } else {
-                    System.out.print("Dealer's cards are: ");
-                    printDealerHand(dealerHand);
-                    System.out.format("Dealer's hand was %d. ", dealerSum);
-                    System.out.println();
-                    System.out.print("\nPlayer's cards are: ");
-                    printCurrentHand(myHand);
-                    System.out.format("\nPlayer's hand was %d. ", sum);
-                    System.out.println("\nPlayer wins!");
+                    System.out.format("%s wins!", "Dealer");
                 }
+                else if (dealerSum == sum) {
+                    System.out.println("Dealer hand is: ");
+                    printDealerHand(dealerHand);
+                    System.out.format("Dealer's hand was %d", dealerSum);
+                    System.out.println("Player hand is: ");
+                    printCurrentHand(myHand);
+                    System.out.format("Player's hand was %d", sum);
+                    System.out.println("It's a tie!");
+                }
+                else if (dealerSum > 21) {
+                    System.out.println("Dealer hand is: ");
+                    printDealerHand(dealerHand);
+                    System.out.format("Dealer's hand was %d", dealerSum);
+                    System.out.format("\nDealer busts! %s wins!", "Player");
+                }
+                else if (dealerSum > sum) {
+                    System.out.println("Dealer hand is: ");
+                    printDealerHand(dealerHand);
+                    System.out.format("Dealer's hand was %d", dealerSum);
+                    System.out.println();
+                    System.out.println("\nPlayer hand is: ");
+                    printCurrentHand(myHand);
+                    System.out.format("\nPlayer's hand was %d", sum);
+                    System.out.format("\n%s wins!", "Dealer");
+                } else {
+                    System.out.println("Dealer hand is: ");
+                    printDealerHand(dealerHand);
+                    System.out.format("Dealer's hand was %d", dealerSum);
+                    System.out.println();
+                    System.out.println("\nPlayer hand is: ");
+                    printCurrentHand(myHand);
+                    System.out.format("\nPlayer's hand was %d", sum);
+                    System.out.format("\n%s wins!", "Player");
+                }
+
                 isPlaying = false;
             } else {
                 System.out.println("\nInvalid Input.");
@@ -133,9 +136,9 @@ public class BlackJack {
     }
 
     public static void printDealerFirst(List<Card> dealerFirst) {
-        System.out.println();
         Card card = dealerFirst.get(0);
-        System.out.println("Dealer's first card is" + " " + card.suit + " - " + card.value);
+        System.out.println("Dealer's first card is: ");
+        System.out.println(card.suit + " - " + card.value );
         System.out.println();
     }
 
@@ -154,3 +157,4 @@ public class BlackJack {
         }
     }
 }
+
